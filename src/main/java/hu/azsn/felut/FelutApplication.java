da0212277e9c2e81c94ca90826403c50f6bc68ee
Abraham Zsombor Nagy <abrahamzsombornagy@gmail.com>
2024-07-22 13:42:34 +0200

    Initial

---
+++ src/main/java/hu/azsn/felut/FelutApplication.java
@@ -0,0 +1,13 @@
+package hu.azsn.felut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FelutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/azsn/felut/FelutApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.azsn.felut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FelutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
